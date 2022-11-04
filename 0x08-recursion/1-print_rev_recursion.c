df9bd8af02fed6e62fe43dbb57bba1e1a362ecf3
Perkunz <ighokarop@gmail.com>
2022-11-03 17:30:41 -0700

    Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: pointer to char s
+ * Return: none
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
