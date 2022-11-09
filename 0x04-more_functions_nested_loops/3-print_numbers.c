fe3fcad255c7fc1d1bb9cd5f0f537c94cb7fb296
Perkunz <ighokarop@gmail.com>
2022-11-08 16:46:44 -0800

    more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
